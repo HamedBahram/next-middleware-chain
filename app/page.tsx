7b6e6a64fbd74e30e1377a6d85d9c56bbc9aba8b
Hamed Bahram <hamed.bahram@gmail.com>
2023-11-02 14:02:53 -0400

    Add next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-export default function Home() {
+import Link from 'next/link'
+
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  const user = session?.user
+
   return (
     <section className='py-24'>
       <div className='container'>
         <h1 className='text-3xl font-bold'>
           Chaining Multiple Middleware Functions
         </h1>
+        <pre className='mt-4'>
+          <code>{JSON.stringify({ name: user?.name }, null, 2)}</code>
+        </pre>
+
+        <Link
+          href='/api/auth/signout'
+          className='mt-4 inline-block rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700'
+        >
+          Sign Out
+        </Link>
       </div>
     </section>
   )
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { chain } from '@/middlewares/chain'
 import { withMiddleware1 } from '@/middlewares/middleware1'
 import { withMiddleware2 } from '@/middlewares/middleware2'
 
-export default chain([withMiddleware1, withMiddleware2])
+// export default chain([withMiddleware1, withMiddleware2])
+
+export { default } from 'next-auth/middleware'
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
