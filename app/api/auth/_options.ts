0c7ec9ab2a2f563565ee91b8c44ae9bd3817b858
Hamed Bahram <hamed.bahram@gmail.com>
2023-11-07 05:37:46 -0500

    Extract next-auth options out of the route handler

---
+++ app/api/auth/_options.ts
@@ -0,0 +1,10 @@
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
+  ]
+}
